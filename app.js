b87509e8c788d56fad29d34cb0ae935488770912
Dan Li <dl.phpf@gmail.com>
2018-04-10 17:23:12 +0800

    started index page

---
+++ app.js
@@ -0,0 +1,38 @@
+//app.js
+App({
+  onLaunch: function () {
+
+  },
+
+  globalData: {
+    pins: [
+      {
+        "id": 1,
+        "avatar": "https://avatars1.githubusercontent.com/u/35441384?s=460&v=4",
+        "author": "DL",
+        "tags": "beach, sugarbeach, toronto",
+        "upvotes": "155",
+        "location": "25 Dockside Dr, Toronto, Ontario M5A 0B5, Canada",
+        "image": "https://static1.squarespace.com/static/55aaed77e4b037ce0ae0cb1f/55aaef44e4b078ade29af75f/55afd2f6e4b0387dc9ed9442/1437586176612/DSC05042.jpg?format=500w"
+      },
+      {
+        "id": 2,
+        "avatar": "https://avatars3.githubusercontent.com/u/7191323?s=460&v=4",
+        "author": "MX",
+        "tags": "teavillage, hangzhou",
+        "upvotes": "123",
+        "location": "Hangzhou, Zhejiang",
+        "image": "https://static1.squarespace.com/static/55aaed77e4b037ce0ae0cb1f/55aaef44e4b078ade29af75f/58bc330117bffc3590658bc6/1506174454674/HZ-4.jpg?format=500w"
+      },
+      {
+        "id": 3,
+        "avatar":"https://avatars3.githubusercontent.com/u/17331094?s=460&v=4",
+        "author": "IC",
+        "tags": "church, uoft",
+        "upvotes": "101",
+        "location": "University of Toronto",
+        "image": "https://static1.squarespace.com/static/55aaed77e4b037ce0ae0cb1f/55aaef44e4b078ade29af75f/57314b904d088edb35b8e97b/1462848432765/UofT-22.jpg?format=1000w"
+      }
+    ]
+  }
+})
