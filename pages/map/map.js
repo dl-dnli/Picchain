13ca9aad62bc618ce643cc8d202fd5534685367e
Dan Li <dl.phpf@gmail.com>
2018-04-12 00:34:51 +0800

    map view codes added

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,66 +1,53 @@
-// pages/map/map.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
-  },
+    // latitude: 28.099994,
+    // longitude: 113.324520,
+    // markers: [{
+    //   id: 1,
+    //   latitude: 28.099994,
+    //   longitude: 113.324520,
+    //   name: 'T.I.T 创意园',
+    //   iconPath: '/images/location.png'
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    // }]
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
   },
+  onLoad: function (e) {
+    const page = this
+    wx.request({
+      url: "http://picchain.herokuapp.com/api/v1/locations",
+      method: 'GET',
+      success(res) {
+        const locations = res.data.locations;
+        let markers = []
+        locations.forEach(function (location) {
+          markers.push({
+            id: location.id,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            name: location.name,
+            iconPath: '/images/location.png'
+          });
+        });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+        page.setData({
+          locations: locations,
+          last_location: {
+            latitude: locations[locations.length - 1].latitude,
+            longitude: locations[locations.length - 1].longitude,
+          },
+          markers: markers
+        });
+        wx.hideToast();
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+      }
+    });
+  }
+  ,
+  onReady: function (e) {
+    let page = this
+    this.mapCtx = wx.createMapContext('myMap')
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
