f0edc8f758548fba23b108df921721e1e16cad34
Leif Copping <seven.minty@yandex.com>
2018-04-12 11:02:40 +0800

    added vendor show

---
+++ pages/vindex/vindex.js
@@ -0,0 +1,90 @@
+// pages/vindex/vindex.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let page = this;
+    wx.request({
+      url: "http://picchain.herokuapp.com/api/v1/locations",
+      method: 'GET',
+      success(res) {
+        const locations = res.data.locations;
+
+        // Update local data
+        page.setData({
+          locations: locations
+        });
+
+        wx.hideToast();
+      }
+    });
+  },
+
+
+  onLocationClicked(e) {
+    const data = e.currentTarget.dataset;
+    const pin = data.pin;
+
+    wx.navigateTo({
+      url: `../show/show?id=${pin.id}`
+    });
+  }, 
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
