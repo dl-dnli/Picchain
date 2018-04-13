83b1014681b6b0646c31982f6ff9791b17bec017
Michael Xu <xu.michaelxia@gmail.com>
2018-04-13 17:16:00 +0800

    populated user show page with pins

--- pages/undex/undex.js
+++ pages/undex/undex.js
@@ -5,46 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "pins": [
-      {
-        "id": 1,
-        "name": "Hummus Hut",
-        "address": "101 Greek Place",
-        "date": "March 7, 2018",
-        "image": "http://askwomenonline.org/wp-content/uploads/2017/12/hummus-recipe-760x428.jpg"
-      },
-      {
-        "id": 2,
-        "name": "Pizza Palace",
-        "address": "90 italian drive",
-        "date": "March 7, 2018",
-        "image": "https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg"
-      },
-      {
-        "id": 3,
-        "name": "Mango Madness",
-        "address": "2 asian court",
-        "date": "March 7, 2018",
-        "image": "https://assets.epicurious.com/photos/57a384e73a12dd9d5602415e/2:1/w_1260%2Ch_630/mango-salad.jpg"
-      },
-      {
-        "id": 4,
-        "name": "Bob's Burger",
-        "address": "42 american drive",
-        "date": "March 7, 2018",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-s/11/76/1c/72/stock-burger-co.jpg"
-      }
-    ]
+    scrollTop: 100,
+    scrollLeft: 0,
+    flag: true,
+    hasLocation: false,
+  },
+
+  show: function () {
+    this.setData({ flag: false })
+  },
+
+  hide: function () {
+
+    this.setData({ flag: true })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    let page = this;
+    wx.request({
+      url: `http://picchain.herokuapp.com/api/v1/users/${options.id}`,
+      method: 'GET',
+      success(res) {
+        const user = res.data
+        const user_pins = res.data.user_pins;
+        const user_locations = res.data.user_locations;
+        // Update local data
+        page.setData({
+          user: user,
+          user_pins: user_pins,
+          user_locations: user_locations
+        });
+
+        wx.hideToast();
+      }
+    });
   },
 
-  addLocation: function() {
+  addLocation: function () {
     wx.redirectTo({
       url: `../location/location?user_id=1`
     });
@@ -54,48 +55,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
