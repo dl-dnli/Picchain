e1a22c1ec035bdbf6b20f31a6601c036ffdc15c9
Leif Copping <seven.minty@yandex.com>
2018-04-13 14:45:41 +0800

    completed user index; static, missing imageboxshadow

--- pages/undex/undex.js
+++ pages/undex/undex.js
@@ -5,7 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    "pins": [
+      {
+        "id": 1,
+        "name": "Hummus Hut",
+        "address": "101 Greek Place",
+        "date": "March 7, 2018",
+        "image": "http://askwomenonline.org/wp-content/uploads/2017/12/hummus-recipe-760x428.jpg"
+      },
+      {
+        "id": 2,
+        "name": "Pizza Palace",
+        "address": "90 italian drive",
+        "date": "March 7, 2018",
+        "image": "https://img.grouponcdn.com/deal/8DDtq5XRzVnLXEUnPHPd/p2-2048x1229/v1/c700x420.jpg"
+      },
+      {
+        "id": 3,
+        "name": "Mango Madness",
+        "address": "2 asian court",
+        "date": "March 7, 2018",
+        "image": "https://assets.epicurious.com/photos/57a384e73a12dd9d5602415e/2:1/w_1260%2Ch_630/mango-salad.jpg"
+      },
+      {
+        "id": 4,
+        "name": "Bob's Burger",
+        "address": "42 american drive",
+        "date": "March 7, 2018",
+        "image": "https://media-cdn.tripadvisor.com/media/photo-s/11/76/1c/72/stock-burger-co.jpg"
+      }
+    ]
   },
 
   /**
